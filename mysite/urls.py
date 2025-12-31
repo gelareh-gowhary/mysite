3719441d0e3770abc1cd56043d0f868db00a2a19
gelareh-gowhary <gelarehgowhary@gmail.com>
2025-12-31 17:28:09 +0330

    created render html files
    added templates folder and html files to website app and render to views

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
-    path('blog/',include('blog.urls'))
+    # path('blog/',include('blog.urls'))
 
 ]
