74001d65cbfc7fa2d1cb99fc6edf37e52ed9719f
gelareh-gowhary <gelarehgowhary@gmail.com>
2026-01-02 23:50:18 +0330

    add author property to Post Class
    add author property to post class as
    a foreignkey

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     # image =
-    # author = 
+    # همراه پاک شدن یوزر پستهای مربطو به اون یوزر هم پاک بشه 
+    # author =models.ForeignKey(User,on_delete=models.CASCADE)
+    author =models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tags = 
