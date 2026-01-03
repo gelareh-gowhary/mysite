254134183fef3399d31e74243d5c688009410fd9
gelareh-gowhary <gelarehgowhary@gmail.com>
2026-01-03 20:05:11 +0330

    use inclusion_tag
    inclusion_tag and create blog-popular-post with use inclusion_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,11 +1,23 @@
 from django import template
 from blog.models import Post
 register=template.Library()
+
 @register.simple_tag(name='totalpost')
 def function():
     posts=Post.objects.filter(status=1).count()
     return posts
+
 @register.simple_tag(name='posts')
 def function():
     posts=Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value,arg=100):
+    return value[:arg] + "......"
+
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts=Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts':posts} 
